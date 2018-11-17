7e8a605c1511143869d39b0ac83a6a732c00864b
ÂngTsúsiông 洪子翔 (a9g) <angtsusiong@gmail.com>
2018-11-17 11:11:26 +0800

    tmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {MaterialModule} from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
